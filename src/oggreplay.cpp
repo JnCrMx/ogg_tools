7a98a1b015dd7e3c3e33c9b0448ddbc680d3eafb
JCM <jacama2009@gmail.com>
2021-06-17 01:55:45 +0200

    Comments to explain what the program is actually doing

--- src/oggreplay.cpp
+++ src/oggreplay.cpp
@@ -30,8 +30,7 @@ OggFrame readFrame(std::vector<char>::iterator& it)
 	OggHeader ogg = *(OggHeader*)&*it;
 	if(std::string(ogg.capture_pattern) != "OggS")
 	{
-		++it;
-		throw std::runtime_error("capture pattern mismatch");
+		throw std::runtime_error("Ogg capture pattern mismatch");
 	}
 
 	it += sizeof(OggHeader);
@@ -73,14 +72,14 @@ AudioAttributes getAudioAttributes(std::vector<char>::iterator it)
 	jt += frame.header.page_segments;
 
 	VorbisHeader vorbis = *(VorbisHeader*)&*jt;
-	
+
 	if(vorbis.packet_type != 0x01 || std::string(vorbis.vorbis) != "vorbis")
 		throw std::runtime_error("illegal vorbis header: packet type "+std::to_string(vorbis.packet_type)+" and signature \""+vorbis.vorbis+"\"");
 
 	return {vorbis.audio_channels, vorbis.audio_sample_rate};
 }
 
-int main(int argc, char* argv[]) 
+int main(int argc, char* argv[])
 {
 	if(argc != 6)
 	{
@@ -115,31 +114,40 @@ int main(int argc, char* argv[])
 	auto audio_attributes2 = getAudioAttributes(data2.begin());
 	if(audio_attributes1 != audio_attributes2)
 	{
-		std::cout << "input1 and input2 differ in sample rate or channel count: " << std::endl << std::left 
+		std::cout << "input1 and input2 differ in sample rate or channel count: " << std::endl << std::left
 			<< "[input1] " << std::setw(std::max(input1.size(), input2.size())) << input1 << " :   " << audio_attributes1.to_string() << std::endl
 			<< "[input2] " << std::setw(std::max(input1.size(), input2.size())) << input2 << " :   " << audio_attributes2.to_string() << std::endl;
 		std::cout << "The output file might not work as expected!" << std::endl;
 	}
 
+	// find last frame of input1
 	OggFrame lastFrame;
 	for(auto it = data1.begin(); it != data1.end();)
 	{
 		lastFrame = readFrame(it);
 	}
 
+	// write entire input1
 	out.write(data1.data(), data1.size());
 
 	auto it = interdata.begin();
-	
+
+	// write first frame of intermediate
 	writeFrame(readFrame(it), out);
+	// write last frame of input1 (already written before)
 	writeFrame(lastFrame, out);
+	// write second frame of intermediate
 	writeFrame(readFrame(it), out);
 
+	// write entire input2
 	out.write(data2.data(), data2.size());
 
+	// write remaining part of intermediate
 	std::vector<char> v(it, interdata.end());
 	out.write(v.data(), v.size());
 
+	// write spacer (a random, relatively long MP3 file to confuse Chromium's duration detection)
 	out.write(spacerdata.data(), spacerdata.size());
+	// write entire input1 again (this is where Chromium will read the total duration of the entire audio file from)
 	out.write(data1.data(), data1.size());
 }
